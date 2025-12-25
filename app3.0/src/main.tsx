3d4b8b6ba4ff79100d1f56789f1e7759eeb8576e
Yug Bhimani <yugbhimani2708@gmail.com>
2025-12-25 18:49:53 +0530

    Fix Netlify white screen (base path)

--- app3.0/src/main.tsx
+++ app3.0/src/main.tsx
@@ -1,7 +1,11 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+const root = document.getElementById("root");
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+createRoot(root).render(<App />);
